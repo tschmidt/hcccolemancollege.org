09fb1f1552fd5058a56c957a10df443eca686076
Terry Schmidt <terry.m.schmidt@gmail.com>
2014-04-04 13:18:07 -0500

    Update ERL RSVP site for 2014

---
+++ erl/js/rsvp.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	$('#erlForm').validate({
+		rules: {
+			'entry.806151579'	: "required", // Full Name
+			'entry.590280394' : "required"  // Program or Department
+		},
+
+		submitHandler: function (form) {
+			var $form = $(form);
+			var form_json = $.parseJSON(JSON.stringify($form.serializeArray()));
+			var $target = $('section > div');
+			submitted = true;
+			form.submit();
+			$form.find('button').attr('disabled', true).text('Submitting...');
+			
+			setTimeout(function() {
+				if (submitted_successful) {
+					$target.html("<h1>Your seat has been reserved!</h1>");
+				}
+			}, 1200);
+		}
+	});
+});
